f0ee717bfc449936c1228d63e0bbbd718022bdf3
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-07-04 15:17:30 +0200

    Update Solution.go

--- Solution.go
+++ Solution.go
@@ -1,110 +1,65 @@
 
-package main
+class Solution {
 
-import (
-    "container/heap"
-    "math"
-)
+    private data class Node(val ID: Int, val weight: Int) {}
 
-const NO_PATH_FOUND = -1
+    private data class Step(val ID: Int, val weightFromStart: Int, val label: Char, val sameConsecutiveLabels: Int) {}
 
-var startID int
-var goalID int
-
-func shortestPath(numberOfNodes int, edges [][]int, labels string, maxSameConsecutiveLabels int) int {
-    goalID = numberOfNodes - 1
-    var directedGraph [][]Node = createDirectedGraph(numberOfNodes, edges)
-    return findShortestPath(directedGraph, labels, maxSameConsecutiveLabels)
-}
-
-func findShortestPath(directedGraph [][]Node, labels string, maxSameConsecutiveLabels int) int {
-    minHeap := PriorityQueue{}
-    heap.Push(&minHeap, Step{startID, 0, labels[startID], 1})
-    minPathWeight := make([][]int, goalID + 1)
-
-    for i := range minPathWeight {
-        minPathWeight[i] = make([]int, maxSameConsecutiveLabels + 1)
-        for j := range minPathWeight[i] {
-            minPathWeight[i][j] = math.MaxInt
-        }
+    private companion object {
+        const val NO_PATH_FOUND = -1
     }
-    minPathWeight[startID][1] = 0
 
-    for minHeap.Len() > 0 {
-        current := heap.Pop(&minHeap).(Step)
-        if current.ID == goalID {
-            return current.weightFromStart
-        }
+    private var startID = 0
+    private var goalID = 0
 
-        for _, next := range directedGraph[current.ID] {
-            if current.label == labels[next.ID] && current.sameConsecutiveLabels + 1 > maxSameConsecutiveLabels {
-                continue
+    fun shortestPath(numberOfNodes: Int, edges: Array<IntArray>, labels: String, maxSameConsecutiveLabels: Int): Int {
+        goalID = numberOfNodes - 1
+        val directedGraph = createDirectedGraph(numberOfNodes, edges)
+        return findShortestPath(directedGraph, labels, maxSameConsecutiveLabels)
+    }
+
+    private fun findShortestPath(directedGraph: Array<MutableList<Node>>, labels: String, maxSameConsecutiveLabels: Int): Int {
+        val minHeap = java.util.PriorityQueue<Step>() { x, y -> x.weightFromStart - y.weightFromStart }
+        minHeap.add(Step(startID, 0, labels[startID], 1))
+
+        val minPathWeight = Array<IntArray>(goalID + 1) { IntArray(maxSameConsecutiveLabels + 1) { Int.MAX_VALUE } }
+        minPathWeight[startID][1] = 0
+
+        while (!minHeap.isEmpty()) {
+            val current = minHeap.poll()
+            if (current.ID == goalID) {
+                return current.weightFromStart
             }
 
-            sameConsecutiveLabels := 1
-            if current.label == labels[next.ID] {
-                sameConsecutiveLabels += current.sameConsecutiveLabels
-            }
+            for (next in directedGraph[current.ID]) {
+                if (current.label == labels[next.ID] && current.sameConsecutiveLabels + 1 > maxSameConsecutiveLabels) {
+                    continue
+                }
 
-            if current.weightFromStart + next.weight >= minPathWeight[next.ID][sameConsecutiveLabels] {
-                continue
-            }
+                var sameConsecutiveLabels = 1
+                if (current.label == labels[next.ID]) {
+                    sameConsecutiveLabels += current.sameConsecutiveLabels
+                }
 
-            minPathWeight[next.ID][sameConsecutiveLabels] = current.weightFromStart + next.weight
-            heap.Push(&minHeap, Step{next.ID, current.weightFromStart + next.weight, labels[next.ID], sameConsecutiveLabels})
+                if (current.weightFromStart + next.weight >= minPathWeight[next.ID][sameConsecutiveLabels]) {
+                    continue
+                }
+
+                minPathWeight[next.ID][sameConsecutiveLabels] = current.weightFromStart + next.weight
+                minHeap.add(Step(next.ID, current.weightFromStart + next.weight, labels[next.ID], sameConsecutiveLabels))
+            }
         }
-    }
-    return NO_PATH_FOUND
-}
-
-func createDirectedGraph(numberOfNodes int, edges [][]int) [][]Node {
-    directedGraph := make([][]Node, numberOfNodes)
-    for node := range numberOfNodes {
-        directedGraph[node] = []Node{}
+        return NO_PATH_FOUND
     }
 
-    for i := range edges {
-        from := edges[i][0]
-        to := edges[i][1]
-        weight := edges[i][2]
-        directedGraph[from] = append(directedGraph[from], Node{to, weight})
+    private fun createDirectedGraph(numberOfNodes: Int, edges: Array<IntArray>): Array<MutableList<Node>> {
+        val directedGraph = Array<MutableList<Node>>(numberOfNodes) { mutableListOf<Node>() }
+        for (i in edges.indices) {
+            val from = edges[i][0]
+            val to = edges[i][1]
+            val weight = edges[i][2]
+            directedGraph[from].add(Node(to, weight))
+        }
+        return directedGraph
     }
-    return directedGraph
-}
-
-type Node struct {
-    ID     int
-    weight int
-}
-
-type Step struct {
-    ID                    int
-    weightFromStart       int
-    label                 byte
-    sameConsecutiveLabels int
-}
-
-type PriorityQueue []Step
-
-func (pq PriorityQueue) Len() int {
-    return len(pq)
-}
-
-func (pq PriorityQueue) Less(first int, second int) bool {
-    return pq[first].weightFromStart < pq[second].weightFromStart
-}
-
-func (pq PriorityQueue) Swap(first int, second int) {
-    pq[first], pq[second] = pq[second], pq[first]
-}
-
-func (pq *PriorityQueue) Push(object any) {
-    step := object.(Step)
-    *pq = append(*pq, step)
-}
-
-func (pq *PriorityQueue) Pop() any {
-    step := (*pq)[pq.Len() - 1]
-    *pq = (*pq)[0 : pq.Len() - 1]
-    return step
 }
